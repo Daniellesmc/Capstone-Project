77b1c8a076d4442ae3a36d1e7a50d97c56806850
Javano Collins <jdcollins242@gmail.com>
2022-12-06 09:19:14 -0500

    fix: duplicate code

--- app/Http/Controllers/Attendance/AttendanceController.php
+++ app/Http/Controllers/Attendance/AttendanceController.php
@@ -33,10 +33,6 @@ class AttendanceController extends Controller
             'student_id' => 'string',
         ]);
 
-        UserAttendance::create([...$data, 'user_id' => $user->id]);
-
-       return redirect()->route('dashboard', ['user' => $user])->withSuccess('Attendance successfull logged!');
-
         $already_logged_today = UserAttendance::where('user_id', $user->id)->whereDate('created_at', Carbon::today())
             ->exists();
 
@@ -44,34 +40,11 @@ class AttendanceController extends Controller
             return redirect()->route('dashboard', ['user' => $user])->with('error', 'You have already logged your attendance today');
         }
 
-
        UserAttendance::create([
             ...$data,
             'user_id' => $user->id,
             'student_id' => $user->id,
         ]);
-
-        return redirect()->route('dashboard', ['user' => $user])->withSuccess('Attendance successful logged!');
-
-        $data = $request->validate([
-            'student_id' => 'string',
-            'first_name' => 'string',
-            'last_name' => 'string',
-
-        ]);
-
-
-        return redirect('/')->with('Success', 'Attendance successfull logged!');
-
-
-
-        $data = $request->validate([
-            'first_name' => 'string',
-            'last_name' => 'string',
-            'student_id' => 'string',
-        ]);
-
-        UserAttendance::create([...$data, 'user_id' => $user->id]);
 
         return redirect()->route('dashboard', ['user' => $user])->withSuccess('Attendance successfull logged!');
 
